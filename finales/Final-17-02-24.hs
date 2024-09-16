ee2703c15426f0b316b731ab3e12b3c8552621a0
Juan manuel iglesias arregui <129231361+JuanmaIglesias10@users.noreply.github.com>
2024-09-16 18:19:08 -0300

    Subo final nuevo

---
+++ finales/Final-17-02-24.hs
@@ -0,0 +1,11 @@
+import Text.Show.Functions()
+
+habilidades :: Persona -> [Habilidad]
+
+sirve :: Problema -> Habilidad -> Bool
+
+{-Definir una funcion que  dado un problema y una lista de personas
+permita saber que personas tienen alguna habilidad que sirva para el
+problema recibido -}
+
+podrianAyudar :: Problema -> [Persona] -> [Persona] = 
